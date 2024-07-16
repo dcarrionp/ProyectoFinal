f1850d396ad38f614bcfc8ae21e37a9323d9cb72
Diego Carrion Portilla <dcportilla2003@gmail.com>
2024-07-15 19:10:00 -0500

    Lista

--- src/main/java/ec/edu/ups/ppw/ProyectoFinal/Model/Prestamo.java
+++ src/main/java/ec/edu/ups/ppw/ProyectoFinal/Model/Prestamo.java
@@ -1,6 +1,5 @@
 package ec.edu.ups.ppw.ProyectoFinal.Model;
 
-import java.io.Serializable;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
@@ -11,42 +10,44 @@ import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 
 @Entity
-public class Prestamo implements Serializable{
-	
-	private static final long serialVersionUID = 1L;
+public class Prestamo {
 
 	@Id
 	private int codigo;
 	@Temporal(TemporalType.DATE)
-    private Date fechaInicio;
-    @Temporal(TemporalType.DATE)
-    private Date fechaFin;
+	private Date fechaInicio;
+	@Temporal(TemporalType.DATE)
+	private Date fechaFin;
 	private String estado;
-	
-	@ManyToOne
-    @JoinColumn(name = "usuario") 
-    private Usuario usuario;
 
-    @ManyToOne
-    @JoinColumn(name = "libro") 
-    private Libro libro;
-	
-	
+	@ManyToOne
+	@JoinColumn(name = "usuario")
+	private Usuario usuario;
+
+	@ManyToOne
+	@JoinColumn(name = "libro")
+	private Libro libro;
+
 	public Date getFechaInicio() {
 		return fechaInicio;
 	}
+
 	public void setFechaInicio(Date fechaInicio) {
 		this.fechaInicio = fechaInicio;
 	}
+
 	public Date getFechaFin() {
 		return fechaFin;
 	}
+
 	public void setFechaFin(Date fechaFin) {
 		this.fechaFin = fechaFin;
 	}
+
 	public String getEstado() {
 		return estado;
 	}
+
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
--- src/main/java/ec/edu/ups/ppw/ProyectoFinal/Model/Usuario.java
+++ src/main/java/ec/edu/ups/ppw/ProyectoFinal/Model/Usuario.java
@@ -1,54 +1,67 @@
 package ec.edu.ups.ppw.ProyectoFinal.Model;
 
-import java.io.Serializable;
 import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 
 @Entity
-public class Usuario implements Serializable{
-	
-	private static final long serialVersionUID = 1L;
-	
+public class Usuario {
+
 	@Id
 	@GeneratedValue
 	private int codigo;
+
 	private String usuario;
 	private String rol;
-	
-	
-	@OneToMany(cascade =  CascadeType.ALL)
+
+	@OneToMany(fetch = FetchType.EAGER)
 	@JoinColumn(name = "historial")
 	private List<Prestamo> historial;
-	
+
 	public List<Prestamo> getHistorial() {
 		return historial;
 	}
+
 	public void setHistorial(List<Prestamo> historial) {
 		this.historial = historial;
 	}
+
 	public int getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
+
 	public String getUsuario() {
 		return usuario;
 	}
+
 	public void setUsuario(String usuario) {
 		this.usuario = usuario;
 	}
+
 	public String isRol() {
 		return rol;
 	}
-	public void setRol(String string) {
-		this.rol = string;
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+	@Override
+	public String toString() {
+		return "usuario [codigo=" + codigo + ", usuario=" + usuario + ", rol=" + rol + ", historial=" + historial + "]";
+	}
+
+	public String getRol() {
+		return rol;
 	}
 
 }
